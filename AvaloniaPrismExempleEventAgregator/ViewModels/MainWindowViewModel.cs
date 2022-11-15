070155a846f8571309a865a149335b0c0cab5c5a
POMXARK <1997pom@gmail.com>
2022-11-15 10:59:28 +1000

    Add project files.

---
+++ AvaloniaPrismExempleEventAgregator/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace AvaloniaPrismExempleEventAgregator.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
